6fffd503253d6842f6a5286ca8eb632a3553cd3b
Zijun Zhou <zzjzhou@ucdavis.edu>
2018-11-02 16:32:52 -0700

    budget controller - store new item data

--- app.js
+++ app.js
@@ -2,7 +2,63 @@
 // buget controller
 var bugetController = (function () {
 
+  // expense object/class constructor
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+  // income object/class constructor
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  }
+
+  // data object to store all useful data
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  // return object
+  return {
+    // add new item method - from input to data storage
+    addItem: function (type, description, value) {
+      var newItem, ID;
+      if (data.allItems[type].length === 0) {
+        ID = 0;
+      } else {
+        // get the id of the last item obj in the array
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      }
+
+      if (type === 'exp') {
+        newItem = new Expense(ID, description, value);
+      } else {
+        newItem = new Income(ID, description, value);
+      }
+
+      // store new item into date
+      data.allItems[type].push(newItem);
+
+      // return new item object
+      return newItem;
+    },
+    // testing method
+    testing: function () {
+      console.log(data);
+    }
+  };
+
 })();
+
 
 // UI controller
 var UIController = (function () {
@@ -54,9 +110,12 @@ var controller = (function (bugetCtrl, UICtrl) {
   // add item callback function
   var ctrlAddItem = function () {
     console.log('button is clicked');
-    // get filed input date
+    var input, newItem;
+    // get filed input data
     console.log(UICtrl.getInput());
-    // add item to budget controller
+    input = UICtrl.getInput();
+    // add item to budget controller - store input data
+    newItem = bugetCtrl.addItem(input.type, input.description, input.value);
     // add item to UI controller
     // calculate budget
     // display budget on UI
